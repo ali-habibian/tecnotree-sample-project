c3230981fc0e8cf9d62a9903b6069907e9879093
Ali Habibian <ali.habibian04@gmail.com>
2022-07-02 17:09:54 +0430

    Initial commit

---
+++ src/main/java/com/habibian/tsp/TecnotreeSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.habibian.tsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnotreeSampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecnotreeSampleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/habibian/tsp/TecnotreeSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habibian.tsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnotreeSampleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
